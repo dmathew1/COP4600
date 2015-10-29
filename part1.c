e5771744f577cd545c6d621eed9e3be177d4b95a
dmathew93 <denzel.mathew@gmail.com>
2015-10-28 20:08:44 -0400

    Finished part 1

--- part1.c
+++ part1.c
@@ -2,52 +2,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
 
-  pid_t sender,receiver;
+pid_t sender,receiver;
+int i;
+char c = 90;
+int fildes[2];
+int isSender,isReceiver;
+size_t readVal;
+char reader;
+
+int main(){
+  printf("Receiever before fork: %d\n",receiver);
+  printf("Sender before fork: %d\n",sender);
+
+
 
   //create pipe
-  int fildes[2];
   pipe(fildes);
 
-  //create sender & receiver processes
+//Forks out the receiver and the sender
+
   receiver = fork();
+  if(receiver==0){
+    isReceiver=1;
+  }
   if(receiver != 0){
     sender = fork();
+    if(sender==0){
+      isSender = 1;
+    }
   }
 
+if(isSender == 0 && isReceiver == 0){
+  close(fildes[1]);
+  close(fildes[0]);
+  sleep(30);
+  printf("FUCK YOU BITCHES: %d", getpid());
+}
 
-  //fildes[1] = write
-  //fildes[0] = read
+  if(isReceiver==1){
 
-  if(receiver==0){
-    printf("I am the receiver: %d\n", getpid());
-
-    //close write buffer
+    //close write
     close(fildes[1]);
-    //read contents
+
+    //read from pipe
+    while(reader != 65){
+      read(fildes[0],&reader,1);
+      write(STDOUT_FILENO,&reader,1);
+    }
+
     //close read
-    //print termination
-    //terminate
-
-    exit(0);
-  }else if (sender==0){
-    printf("I am the sender: %d\n", getpid());
-
-    //close read buffer
     close(fildes[0]);
 
-    //write contents Reverse Alphabet
-    for(int i=65;i<91;i++){
-      char c = i;
-      printf("%c\n",c);
-
-    }
-    //close write
     //print termination
-    //terminate
+    printf("\nFUCK YOUUUUUUU RECEIVER GOING OUT: %d", getpid());
 
+    //terminate
     exit(0);
+
+  }else if (isSender==1){
+
+    //close read
+    close(fildes[0]);
+
+    //write to pipe
+    for(i=90;i>64;--i){
+      write(fildes[1],&i,1);
+      sleep(1);
+    }
+
+    //close write
+    close(fildes[1]);
+
+    //print termination
+    printf("\nFUCK YOU SENDER: %d", getpid());
+    fflush(stdout);
+
+    //terminate
+    exit(0);
+
   }
 
   return 0;
